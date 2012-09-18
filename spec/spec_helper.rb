33545e910b879c52ef3611e3baefd43a447a41fc
Colin Tremblay <tremblay@grinnell.edu>
2012-09-17 19:26:03 -0500

    done with static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -71,4 +71,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  end
+
+
+Spork.each_run do
 end
