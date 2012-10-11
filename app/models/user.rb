53ec3009855d2f80ce5892778edb3533b8fe2695
Colin Tremblay <tremblay@grinnell.edu>
2012-10-10 22:13:33 -0500

    Added user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,13 @@ class User < ActiveRecord::Base
  # attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
   has_many :microposts, :dependent => :destroy
+  has_many :relationships, :foreign_key => "follower_id",
+                           :dependent => :destroy
+  has_many :following, :through => :relationships, :source => :followed
+  has_many :reverse_relationships, :foreign_key => "followed_id",
+                                   :class_name => "Relationship",
+                                   :dependent => :destroy
+  has_many :followers, :through => :reverse_relationships, :source => :follower
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
@@ -43,9 +50,20 @@ end
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
   end
-    def feed
-    # This is preliminary. See Chapter 12 for the full implementation.
-    Micropost.where("user_id = ?", id)
+  def following?(followed)
+    relationships.find_by_followed_id(followed)
+  end
+
+  def follow!(followed)
+    relationships.create!(:followed_id => followed.id)
+  end
+
+  def unfollow!(followed)
+    relationships.find_by_followed_id(followed).destroy
+  end
+
+  def feed
+    Micropost.from_users_followed_by(self)
   end
   private
 
