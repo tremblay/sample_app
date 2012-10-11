b5e0897453e7c963caa38d82d858b5fbb2c94f7b
Colin Tremblay <tremblay@grinnell.edu>
2012-10-10 21:37:50 -0500

    Added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,7 +43,10 @@ end
     user = find_by_id(id)
     (user && user.salt == cookie_salt) ? user : nil
   end
-  
+    def feed
+    # This is preliminary. See Chapter 12 for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
   private
 
     def create_remember_token
